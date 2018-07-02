66beb92a94f160dc8a93f49e91e043057894bf8f
Lachlan Campbell <lachlanjc@users.noreply.github.com>
2018-07-02 15:11:49 -0700

    👍

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import { Text } from '@hackclub/design-system'
 import LoadingBar from './LoadingBar'
 import ErrorPage from './ErrorPage'
 import Project from './Project'
-const axios = require('axios');
+import axios from 'axios'
 
 class Projects extends Component {
   state = {
@@ -14,30 +14,41 @@ class Projects extends Component {
   }
 
   loadProjects() {
-    const { authKey } = this.props;
-    if(authKey) {
+    const { authKey } = this.props
+    if (authKey) {
       const api = axios.create({
         baseURL: 'https://hackchicago.herokuapp.com/api/',
-        headers: {'Auth': authKey}
-      });
-      api.get('v1/projects')
+        headers: { Auth: authKey }
+      })
+      api
+        .get('v1/projects')
         .then(res => {
-          this.setState({ projects: res.data, status: 'success', message: res.statusText });
+          this.setState({
+            projects: res.data,
+            status: 'success',
+            message: res.statusText
+          })
         })
         .catch(error => {
-          this.setState({ message: error.response.data.message, status: 'error' });
+          this.setState({
+            message: error.response.data.message,
+            status: 'error'
+          })
         })
     } else {
-      this.setState({ message: 'No authentication key provided', status: 'error' });
+      this.setState({
+        message: 'No authentication key provided',
+        status: 'error'
+      })
     }
   }
 
   componentDidMount() {
-    this.loadProjects();
+    this.loadProjects()
 
     this.refreshIntervalId = setInterval(() => {
       this.loadProjects()
-    }, 4096)
+    }, 4000)
   }
 
   componentWillUnmount() {
@@ -52,26 +63,22 @@ class Projects extends Component {
       case 'success':
         return (
           <Fragment>
-            {projects.length < 1 && (
+            {projects.length < 1 ? (
               <Text f={3} color="muted" py={4} align="center" bold>
                 No projects yet!
               </Text>
+            ) : (
+              projects.map(project => (
+                <Project name={project.name} key={project._id} />
+              ))
             )}
-            {projects.map(project => (
-              <Project
-                name={project.name}
-                key={project._id}
-              />
-            ))}
           </Fragment>
         )
       case 'error':
         return (
-          <Fragment>
-            <Text f={3} color="error" py={4} align="center" bold>
-              Error: {message}
-            </Text>
-          </Fragment>
+          <Text f={3} color="error" py={4} align="center" bold>
+            Error: {message}
+          </Text>
         )
       default:
         return <ErrorPage />
