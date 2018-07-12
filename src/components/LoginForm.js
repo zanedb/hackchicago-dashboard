35558ea8e0aaf6af01faa7f8c45fb19c09ec1198
Zane <github@zane.sh>
2018-07-12 00:40:18 -0700

    Use new login system

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,20 +5,21 @@ import { Text } from '@hackclub/design-system'
 
 class LoginForm extends Component {
   state = {
-    status: this.props.status
+    status: this.props.status,
+    loginCodeSent: false
   }
 
   render() {
-    const { status } = this.state
+    const { status, loginCodeSent } = this.state
     return (
       <div>
         {status === '' ? (
           <Formik
             initialValues={{
-              email: ''
+              email: '',
+              token: ''
             }}
             validate={values => {
-              // same as above, but feel free to move this into a class method now.
               let errors = {}
               if (!values.email) {
                 errors.email = 'Required'
@@ -31,32 +32,62 @@ class LoginForm extends Component {
             }}
             onSubmit={(
               values,
-              { setSubmitting, setErrors /* setValues and other goodies */ }
+              { setSubmitting, setErrors }
             ) => {
-              axios({
-                method: 'post',
-                url: 'http://localhost:8080/v1/auth/sendtoken',
-                data: `user=${values.email}`
-              })
-                .then(res => {
-                  console.log(res)
-                  setSubmitting(true)
-                  this.setState({
-                    status: res.data.message
-                  })
-                })
-                .catch(error => {
-                  console.log(error.response)
-                  if (error.response.status === 401) {
-                    this.setState({
-                      status: 'This email is not signed up.'
-                    })
-                  } else {
-                    this.setState({
-                      status: error.response.statusText
-                    })
+              if (values.token === '') {
+                axios({
+                  method: 'post',
+                  url: 'http://localhost:8080/auth',
+                  data: {
+                    email: values.email
                   }
                 })
+                  .then(res => {
+                    setSubmitting(false)
+                    this.setState({
+                      loginCodeSent: true
+                    })
+                  })
+                  .catch(error => {
+                    if (error.response.status === 401) {
+                      this.setState({
+                        status: 'This email is not signed up.'
+                      })
+                    } else {
+                      this.setState({
+                        status: error.response.statusText
+                      })
+                    }
+                  })
+              } else {
+                axios({
+                  method: 'post',
+                  url: 'http://localhost:8080/auth',
+                  data: {
+                    email: values.email,
+                    token: values.token
+                  }
+                })
+                  .then(res => {
+                    console.log(res)
+                    setSubmitting(true)
+                    this.setState({
+                      status: res.data.message
+                    })
+                  })
+                  .catch(error => {
+                    console.log(error.response)
+                    if (error.response.status === 401) {
+                      this.setState({
+                        status: 'This email is not signed up.'
+                      })
+                    } else {
+                      this.setState({
+                        status: error.response.statusText
+                      })
+                    }
+                  })
+              }
             }}
             render={({
               values,
@@ -76,6 +107,23 @@ class LoginForm extends Component {
                   onBlur={handleBlur}
                   value={values.email}
                 />
+                {loginCodeSent === true ? (
+                  <div>
+                    <Text f={3} color="accent" py={4} bold>
+                      Please check your email, and enter the token.
+                    </Text>
+                    <input
+                      type="token"
+                      name="token"
+                      placeholder="000000"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.token}
+                    />
+                  </div>
+                ) : (
+                  <div />
+                )}
                 {touched.email && errors.email && <div>{errors.email}</div>}
                 <button type="submit" disabled={isSubmitting}>
                   Submit
