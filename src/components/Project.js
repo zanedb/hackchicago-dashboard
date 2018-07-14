0fb2ae5886447b023a5002f22f1678f250181ff1
Zane <github@zane.sh>
2018-07-14 01:59:53 -0700

    Improve margins

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,8 +6,10 @@ class Project extends Component {
     const { name, description, submitter, tagline, timestamp } = this.props
     return (
       <Card boxShadowSize="sm" my={3} p={3} color="black" bg="white">
-        <Heading m={3}>{name}</Heading>
-        <Text m={3} fontSize={2}>
+        <Heading m={3} mb={1}>
+          {name}
+        </Heading>
+        <Text m={3} fontSize={2} mt={1}>
           Submitted by {submitter.email}
         </Text>
         <Text m={3} fontSize={1}>
