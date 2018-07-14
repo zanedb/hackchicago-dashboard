bfa1c1b541b8d98298d881c64c4eabd2ec89b782
Zane <github@zane.sh>
2018-07-14 01:01:26 -0700

    Use DS buttons

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Button } from '@hackclub/design-system'
 
 class LogoutButton extends Component {
   logout = () => {
@@ -21,7 +22,7 @@ class LogoutButton extends Component {
   }
 
   render() {
-    return <button onClick={this.logout}>Logout</button>
+    return <Button onClick={this.logout} bg='accent'>Logout</Button>
   }
 }
 
