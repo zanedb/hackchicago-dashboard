69c4c3b990f30186fc702b92d6fe30f985934ef9
Zane <github@zane.sh>
2018-07-20 00:21:46 -0700

    Use functional components

--- src/components/admin/ExtendedAttendee.js
+++ src/components/admin/ExtendedAttendee.js
@@ -24,58 +24,46 @@ const FlexHeading = Flex.withComponent(Heading.h2).extend`
   text-align: left;
 `
 
-class ExtendedAttendee extends Component {
-  render() {
-    const {
-      fname,
-      lname,
-      role,
-      grade,
-      school,
-      city,
-      state,
-      email,
-      phone,
-      parentName,
-      parentEmail,
-      parentPhone
-    } = this.props.attendee
-    return (
-      <Fragment>
-        <FlexHeading fontSize={6} mb={2}>
-          {fname} {lname}
-          <Badge
-            bg={role === 'admin' ? 'important' : 'smoke'}
-            color={role === 'admin' ? 'white' : 'slate'}
-            ml={['auto', 2]}
-          >
-            {role}
-          </Badge>
-        </FlexHeading>
-        <Heading f={4} mb={3}>
-          {grade}th Grader at {school} in {city}, {state}
-        </Heading>
-        <Text f={4}>
-          Email: <Link href={`mailto:${email}`}>{email}</Link>
-          {phone !== '' && (
-            <Fragment>
-              <br />Phone: <Link href={`tel:${phone}`}>{phone}</Link>
-            </Fragment>
-          )}
-        </Text>
-        <Heading.h2 f={5} my={3}>
-          Parent/Guardian
-        </Heading.h2>
-        <Text f={4}>
-          Name: {parentName}
-          <br />
-          Email: <Link href={`mailto:${parentEmail}`}>{parentEmail}</Link>
-          <br />
-          Phone: <Link href={`tel:${parentPhone}`}>{parentPhone}</Link>
-        </Text>
-      </Fragment>
-    )
-  }
-}
+const ExtendedAttendee = ({ attendee }) => (
+  <Fragment>
+    <FlexHeading fontSize={6} mb={2}>
+      {attendee.fname} {attendee.lname}
+      <Badge
+        bg={attendee.role === 'admin' ? 'important' : 'smoke'}
+        color={attendee.role === 'admin' ? 'white' : 'slate'}
+        ml={['auto', 2]}
+      >
+        {attendee.role}
+      </Badge>
+    </FlexHeading>
+    <Heading f={4} mb={3}>
+      {attendee.grade}th Grader at {attendee.school} in {attendee.city},{' '}
+      {attendee.state}
+    </Heading>
+    <Text f={4}>
+      Email: <Link href={`mailto:${attendee.email}`}>{attendee.email}</Link>
+      {attendee.phone !== '' && (
+        <Fragment>
+          <br />Phone:{' '}
+          <Link href={`tel:${attendee.phone}`}>{attendee.phone}</Link>
+        </Fragment>
+      )}
+    </Text>
+    <Heading.h2 f={5} my={3}>
+      Parent/Guardian
+    </Heading.h2>
+    <Text f={4}>
+      Name: {attendee.parentName}
+      <br />
+      Email:{' '}
+      <Link href={`mailto:${attendee.parentEmail}`}>
+        {attendee.parentEmail}
+      </Link>
+      <br />
+      Phone:{' '}
+      <Link href={`tel:${attendee.parentPhone}`}>{attendee.parentPhone}</Link>
+    </Text>
+  </Fragment>
+)
 
 export default ExtendedAttendee
