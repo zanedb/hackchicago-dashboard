59d0b6735cdbf3928bb413431b3740079c176bce
Zane <github@zane.sh>
2018-07-21 07:48:20 -0700

    Add waiver status

--- src/pages/attendee.js
+++ src/pages/attendee.js
@@ -1,5 +1,12 @@
-import React, { Component, Fragment } from 'react'
-import { Container, Text, Box, Flex, Button } from '@hackclub/design-system'
+import React, { Component } from 'react'
+import {
+  Container,
+  Text,
+  Box,
+  Flex,
+  Button,
+  Heading
+} from '@hackclub/design-system'
 import axios from 'axios'
 
 import ExtendedAttendee from './../components/admin/ExtendedAttendee'
@@ -7,6 +14,34 @@ import LoadingBar from './../components/LoadingBar'
 import ErrorPage from './../components/ErrorPage'
 import Header from './../components/Header'
 import EditAttendee from './../components/admin/EditAttendee'
+
+const Switch = Box.extend`
+  border-radius: 99999px;
+  display: inline-flex;
+  width: 40px;
+  height: 24px;
+  background-color: ${props =>
+    props.checked ? props.theme.cx(props.color) : 'transparent'};
+  box-shadow: inset 0 0 0 2px;
+  transition-property: background-color;
+  transition-duration: 0.25s;
+  transition-timing-function: ease-out;
+  user-select: none;
+  &:after {
+    content: ' ';
+    width: 16px;
+    height: 16px;
+    margin: 4px;
+    border-radius: 8px;
+    background-color: ${props =>
+      props.checked ? props.theme.colors.white : props.theme.cx(props.color)};
+    transition-property: transform, color;
+    transition-duration: 0.125s;
+    transition-timing-function: ease-out;
+    transform: ${props =>
+      props.checked ? `translateX(16px)` : `translateX(0)`};
+  }
+`
 
 class Project extends Component {
   state = {
@@ -67,6 +102,52 @@ class Project extends Component {
       })
   }
 
+  setParentWaiver(status, id) {
+    console.log('par')
+    axios
+      .put(
+        `https://api.hackchicago.io/v1/attendees/id/${id}`,
+        {
+          waiverStatus: {
+            parent: status
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(res => {
+        this.setState(({ attendee }) => {
+          attendee.waiverStatus.parent = status
+          return { attendee }
+        })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  setStudentWaiver(status, id) {
+    console.log('stu')
+    axios
+      .put(
+        `https://api.hackchicago.io/v1/attendees/id/${id}`,
+        {
+          waiverStatus: {
+            student: status
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(res => {
+        this.setState(({ attendee }) => {
+          attendee.waiverStatus.student = status
+          return { attendee }
+        })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
   render() {
     const { attendee, status, view } = this.state
     switch (status) {
@@ -115,6 +196,39 @@ class Project extends Component {
                   <Button bg="muted" onClick={this.viewAdmin}>
                     Cancel
                   </Button>
+                </Flex>
+                <Flex flexDirection="column" mt={3}>
+                  <Heading.h2>Check-in</Heading.h2>
+                  <Flex align="center" my={3}>
+                    <Switch
+                      color="primary"
+                      role="checkbox"
+                      checked={attendee.waiverStatus.student}
+                      onClick={() => {
+                        this.setStudentWaiver(
+                          !attendee.waiverStatus.student,
+                          attendee.id
+                        )
+                      }}
+                      mr={2}
+                    />
+                    <Text>Student Waiver Status</Text>
+                  </Flex>
+                  <Flex align="center">
+                    <Switch
+                      color="primary"
+                      role="checkbox"
+                      checked={attendee.waiverStatus.parent}
+                      onClick={() => {
+                        this.setParentWaiver(
+                          !attendee.waiverStatus.parent,
+                          attendee.id
+                        )
+                      }}
+                      mr={2}
+                    />
+                    <Text>Parent Waiver Status</Text>
+                  </Flex>
                 </Flex>
               </Container>
             )}
